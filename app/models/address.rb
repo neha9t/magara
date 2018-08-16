ead13ca90623f2a86d7cb7736be09492c14fca30
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2018-08-16 15:23:24 -0700

    :card_file_box: add attribute of presence, length and RegEx format

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :house
+
+  validates :address_1, presence: true
+  validates :city,      presence: true
+  # HACK:
+  #   Only save upcase state abbr name
+  validates :state,     presence: true, length: { is: 2 }
+  # HACK:
+  #   The RegEx validation is needed to be refactored
+  validates :zip_code,  presence: true, format: { with: /\A9[0-6]\d{3}\z/ }
 end
