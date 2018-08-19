052af0a0dcfd0f6cf2ac77b021997af7f0d221c4
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2018-08-19 03:03:24 -0700

    :white_check_mark: add checkbox spec

--- spec/models/checkbox_spec.rb
+++ spec/models/checkbox_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Checkbox, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Checkbox do
+  let(:checkbox) { create(:checkbox, house: build(:house)) }
+
+  it 'should be valid' do
+    expect(checkbox).to be_valid
+  end
+
+  # All of them true because of the beautiy of Active Record <3
+  # AR returns to true them except the +nil+.
+  ["true", 1, nil].each do |lol|
+    it "should not be valid if it has #{lol}" do
+      checkbox.air_conditioning = lol
+      expect(checkbox).to be_valid
+    end
+  end
 end
