bbaba54a1dfbe077b9bac50b4009ab8292512760
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2018-08-19 13:46:52 -0700

    :white_check_mark: add HousesController spec for index and show

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe HousesController do
+  context 'GET #index' do
+    before { get :index }
+    it     { is_expected.to render_template('index') }
+
+    it 'assigns @houses' do
+      create(:house)
+      expect(assigns(:houses)).to eq(House.all)
+    end
+  end
+
+  context 'GET #show' do
+    before do
+      h = build(:house)
+      create(:address, house: h)
+      create(:checkbox, house: h)
+    end
+
+    before { get :show, params: { id: House.first.id } }
+    it     { is_expected.to render_template('show') }
+  end
+
+  context 'GET #new'
+  context 'POST #create'
+  context 'GET #edit'
+  context 'PATCH/PUT #update'
+  context 'DELETE #destroy'
 end
